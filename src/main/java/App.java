02033e5c09d4161f90736d2adbb733a0ecfbb881
Alfonso Noerenberg Junior <alfonsojunior@gmail.com>
2017-12-09 15:28:25 -0200

    Jenkins

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,16 +1,9 @@
-import java.util.ArrayList;
+import java.util.logging.Logger;
 
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		ArrayList<Integer> books = new ArrayList<Integer>();
-		books.add(1);
-		books.add(2);
-		books.add(1);
-		books.add(3);
-		BookStore store = new BookStore();
-		store.calculateBasketCost(books);
+		Logger.getGlobal().info("Teste");
 	}
 
 }
--- src/main/java/BookStore.java
+++ src/main/java/BookStore.java
@@ -1,29 +1,39 @@
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class BookStore {
 
-	private static final double preco = 8.00;
-	
-	public BookStore() {
-		
-	}
+	private static final double PRECO = 8.00;
 	
 	public double calculateBasketCost(List<Integer> books) {
 		
-		double retorno = 0.00;
+		double retorno;
 		
-		Set<Integer> unique = new HashSet<Integer>(books);
-		//System.out.println(unique);
-		/*
-		for (Integer key : unique) {
-		    System.out.println(key + ": " + Collections.frequency(books, key));
+		Set<Integer> unique = new HashSet<>(books);
+		int counter = unique.size();
+		retorno = counter * PRECO;
+		retorno = this.aplicarDesconto(retorno, counter);
+		int qtd = (books.size() - counter);
+		retorno += this.aplicarDesconto(qtd * PRECO, qtd);
+		
+		return retorno;
+		
+	}
+	
+	private double aplicarDesconto(double valor, int qtd) {
+		
+		double retorno = valor;
+		
+		if (qtd == 2) {
+			retorno = valor - (valor * 0.05);
+		} else if (qtd == 3) {
+			retorno = valor - (valor * 0.10);
+		} else if (qtd == 4) {
+			retorno = valor - (valor * 0.20);
+		} else if (qtd >= 5) {
+			retorno = valor - (valor * 0.25);
 		}
-		*/
-		retorno = books.size() * preco;
 		
 		return retorno;
 		
